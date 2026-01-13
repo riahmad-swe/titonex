d5f5b5fa44f4cb1818742542a667254e25622ae5
riaurko <riaurko@gmail.com>
2026-01-13 19:50:54 +0600

    feat: add og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,30 @@
-"use client";
-
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import { Metadata } from "next";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const metadata: Metadata = {
+	metadataBase: new URL("https://titonex.com"),
+	title: "Titonex",
+	openGraph: {
+		type: "website",
+		siteName: "Titonex",
+		locale: "en_US",
+		images: [
+			{
+				url: "/og/home.png",
+				width: 1200,
+				height: 550,
+				alt: "Titonex Software Solutions",
+			},
+		],
+	},
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
 	return (
 		<html
 			lang="en"
